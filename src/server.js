2ac15a6a7e56a4304097f7ead6d656b64f738919
CampossCaio <caiocamposs141097@gmail.com>
2019-10-04 12:22:28 -0300

    backend-final

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes =  require('./routes');
-
+const cors = require('cors');
 const app = express();
-app.use(express.json());
+
                                //usuári0 : senha                          //nome da base de dados
 mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-8jkd4.mongodb.net:27017,cluster0-shard-00-01-8jkd4.mongodb.net:27017,cluster0-shard-00-02-8jkd4.mongodb.net:27017/semana09?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -19,6 +19,9 @@ mongoose.connect('mongodb://omnistack:omnistack@cluster0-shard-00-00-8jkd4.mongo
 
 //Para a aplicação aceitar requisições Json
 
+//cors controla quais urls poderão requisitar nossa api
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(8081);
